0065126e44f96c47cfdbea7c8ce391bf3f658ece
A Bergelin <the_bergelin@hotmail.co.uk>
2021-05-07 14:36:26 +0100

    Remove border

--- 2.hexColourPicker/app.js
+++ 2.hexColourPicker/app.js
@@ -1,28 +1,19 @@
-// let hexNumber = document.querySelector('body').style.backgroundColor;
-// let hexNumber = document.body.style.backgroundColor;
-// console.log(hexNumber);
-// alert(document.body.style.backgroundColor);
 
-// document.querySelector('p').textContent = '3';
-
+// Getting button element
+let button = document.getElementById('btn');
 const hexNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
 function generateHexColour(){
     let hex = "#";
+    // generating hexadecimal number at random
     for (let i = 0; i < 6; i++) {
         hex += hexNumber[Math.floor(Math.random()*hexNumber.length)];
     }
     document.body.style.backgroundColor = hex;
-    document.querySelector('p').textContent = hex;
+    document.querySelector('#hexValue').textContent = hex;
 }
 
 
-
-// function changeBackground(){
-//     document.body.style.backgroundColor = '#456';
-// }
-
-let button = document.getElementById('btn');
 button.addEventListener("click", function(){
     generateHexColour();
 });
