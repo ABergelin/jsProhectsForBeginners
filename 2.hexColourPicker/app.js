7bf5a170368e3453d72746f9803f2e7d4b1f7183
A Bergelin <the_bergelin@hotmail.co.uk>
2021-05-07 14:58:26 +0100

    Remove background for button

--- 2.hexColourPicker/app.js
+++ 2.hexColourPicker/app.js
@@ -1,4 +1,3 @@
-
 // Getting button element
 let button = document.getElementById('btn');
 const hexNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
@@ -12,7 +11,6 @@ function generateHexColour(){
     document.body.style.backgroundColor = hex;
     document.querySelector('#hexValue').textContent = hex;
 }
-
 
 button.addEventListener("click", function(){
     generateHexColour();
